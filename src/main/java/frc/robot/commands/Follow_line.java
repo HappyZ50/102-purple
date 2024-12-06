36bdbcd26902caf97a4161055ee18e8e29dfe616
FRCuser <FRCuser@fakeemail.com>
2024-12-05 20:52:57 -0600

    LineFollow

--- src/main/java/frc/robot/commands/Follow_line.java
+++ src/main/java/frc/robot/commands/Follow_line.java
@@ -8,15 +8,24 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.xrp.XRPReflectanceSensor;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.XRPDrivetrain;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Follow_line extends Command {
-  XRPReflectanceSensor linesensor = new XRPReflectanceSensor(); 
+  private XRPReflectanceSensor linesensor = new XRPReflectanceSensor(); 
+  private XRPDrivetrain m_Drivetrain;
+  
+  }
   /** Creates a new Follow_line. */
-  public Follow_line() {
+  public Follow_line(XRPDrivetrain drivetrain){
     // Use addRequirements() here to declare subsystem dependencies.
+    
+      m_Drivetrain = drivetrain;
+      addRequirements(drivetrain);
     SmartDashboard.putData(this);
   }
+
+  
   @Override
   public void initSendable(SendableBuilder builder){
     super.initSendable(builder); //send data to driver station.
